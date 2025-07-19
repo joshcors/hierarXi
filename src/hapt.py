191024c96652e03dd7a9b00e3616f09d2b270eb1
Joshua Corsello <joshcors@umich.edu>
2025-07-19 03:15:47 -0700

    decreased mem overhead

--- src/hapt.py
+++ src/hapt.py
@@ -22,19 +22,30 @@ class HAPT(KMeans):
 
     Hierarchical K-means tree, with a unique PCA projection on each graph edge
     """
-    def __init__(self, n_clusters, is_leaf, parent, points, base_dir=None):
+    def __init__(self, n_clusters, is_leaf, parent=None, points=None, base_dir=None, root=None, start_idx=None, end_idx=None, centroid=None):
         """
         n_clusters  : k
         is_leaf     : denotes if tree is leaf node
         parent      : parent HAPT object
         points      : numpy array of points for node
         """
+        self.root = root
+        if self.root is None:
+            self.root = self
+
         self.is_leaf:bool        = is_leaf
         self.parent:HAPT         = parent
         self.n_clusters:int      = n_clusters
-        self.points:np.ndarray   = points
-        self.centroid:np.ndarray = self.points.mean(axis=0)
         self.base_dir:str        = base_dir
+        self.centroid            = centroid
+        self.points:np.ndarray   = points
+
+        self.start_idx = start_idx
+        self.end_idx = end_idx
+
+        if self.points is not None:
+            self.start_idx = 0
+            self.end_idx = len(self.points)
 
         self.cumulated_projection:np.ndarray = None
         self.projection:np.ndarray           = None
@@ -43,30 +54,57 @@ class HAPT(KMeans):
 
         super().__init__(n_clusters=n_clusters, **DEFAULT_KMEANS_KWARGS)
 
+    def get_points(self):
+        """Get points from root based on limit `idx`s"""
+        return self.root.points[self.start_idx:self.end_idx]
+    
+    def get_projected_points(self):
+        """Get points projected into cluster's subspace"""
+        return self.get_points() @ self.cumulated_projection
+    
+    def get_projected_centroid(self):
+        """Get cluster centroid projected into cluster's subspace"""
+        return self.centroid @ self.cumulated_projection
+    
+    def sort_points(self, start, end, order):
+        """Sort portion of `root`'s points (from `start` to `end`), in `order`"""
+        self.root.points[start:end] = self.root.points[start:end][order]
+        
     def cluster_fit(self):
         """
         Fit k-means cluster on points, extract clusters
         """
         
-        labels = self.fit_predict(self.points)
+        # Get K-means labels
+        labels = self.fit_predict(self.get_points())
 
-        clusters = [
-            self.points[labels == i] for i in range(self.n_clusters)
-        ]
+        # Sort points by clusters
+        sort_idx = np.argsort(labels)
+        counts = np.bincount(labels, minlength=self.n_clusters)
+        cutoffs = np.concat(([0, ], np.cumsum(counts))) + self.start_idx
+        self.root.sort_points(self.start_idx, self.end_idx, sort_idx)
+
+        # Get cutoff indices (points[start:end]) for each cluster
+        idx = [(cutoffs[j], cutoffs[j + 1]) for j in range(self.n_clusters)]
         
-        return clusters
+        return idx
     
     def branch(self, depth_remaining):
         """
         Recursively generate clusters
         """
-        clusters = self.cluster_fit()
+        idx = self.cluster_fit()
+        centroids = self.cluster_centers_
 
         is_leaf = (depth_remaining == 1)
 
         for i in range(self.n_clusters):
-            self.branches[i] = HAPT(self.n_clusters, is_leaf, self, clusters[i])
+            start, end = idx[i]
+            if start == end: continue
 
+            self.branches[i] = HAPT(self.n_clusters, is_leaf, parent=self, root=self.root, start_idx=start, end_idx=end, centroid=centroids[i])
+
+            # Recurse non-leaves
             if not is_leaf:
                 self.branches[i].branch(depth_remaining - 1)
 
@@ -77,16 +115,19 @@ class HAPT(KMeans):
         """
 
         # Extract and project points for use
-        points_use = self.points.copy()
+        points_use = self.get_points().copy()
         if self.parent is not None:
             points_use = points_use @ self.parent.cumulated_projection
 
         # Get properly-sized projection
         #   Note: this may result in nodes with lower-dimension vectors
-        q = min(sizes[0], points_use.shape[1], points_use.shape[0] - 1)
-        _, _, V = pca_lowrank(torch.tensor(points_use), q=q)
-
-        self.projection = V.numpy()
+        n, Din = points_use.shape
+        q = min(sizes[0], Din, n - 1)
+        if q < 1:
+            self.projection = np.eye(Din, dtype=np.float32)
+        else:
+            _, _, V = pca_lowrank(torch.from_numpy(points_use).half(), q=q)
+            self.projection = V.numpy()
 
         # Accumulate projections
         if self.parent is None:
@@ -102,20 +143,25 @@ class HAPT(KMeans):
             self.branches[i].get_projections(sizes[1:])
 
     def save_vectors(self, base_dir=None):
+        """Save projected points and centroid to individual `.npy` files"""
+
+        # Check for base_dir somewhere
         if base_dir is None:
             base_dir = self.base_dir
         if base_dir is None:
             print("Please provide a directory")
             return
         
-        points_use = self.points @ self.cumulated_projection
 
-        np.save(os.path.join(base_dir, "points.npy"), points_use)
+        # Save
+        if self.is_leaf:
+            np.save(os.path.join(base_dir, "points.npy"), self.get_projected_points())
         try:
-            np.save(os.path.join(base_dir, "centroids.npy"), self.cluster_centers_)
+            np.save(os.path.join(base_dir, "centroid.npy"), self.get_projected_centroid())
         except AttributeError:
             return
 
+        # Recurse
         for i in range(self.n_clusters):
             _dir = os.path.join(base_dir, f"{i}")
             if not os.path.exists(_dir):
@@ -123,14 +169,17 @@ class HAPT(KMeans):
             self.branches[i].save_vectors(_dir)
 
     def save_compressed(self, data, file_path):
-        points_use = self.points @ self.cumulated_projection
+        """Save projected vectors and centroids in compressed `.npz`"""
 
-        data["points"] = points_use
+        # Record
+        if self.is_leaf:
+            data["points"] = self.get_projected_points()
         try:
-            data["centroids"] = self.cluster_centers_
+            data["centroid"] = self.get_projected_centroid()
         except AttributeError:
             return 
         
+        # Recurse
         for i in range(self.n_clusters):
             data[i] = {}
             self.branches[i].save_compressed(data[i], file_path)
